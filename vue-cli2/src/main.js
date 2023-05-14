c8040dd53d5cf8d4e18be6f524c7ee51b5f2fe70
Eerang2 <117508537+Eerang2@users.noreply.github.com>
2023-05-14 15:51:28 +0900

    cli 사용하여 vue.js 응용

--- vue-cli2/src/main.js
+++ vue-cli2/src/main.js
@@ -6,3 +6,11 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// 위 코드를 풀면 아래와 같다
+// new Vue({
+//   el: '#app',
+//   components: {
+//     'app': App
+//   }
+// })
