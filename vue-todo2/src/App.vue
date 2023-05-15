b60666d48b523a87a420ba156e7ed8e994310f30
Eerang2 <117508537+Eerang2@users.noreply.github.com>
2023-05-15 20:56:49 +0900

    vue todo

---
+++ vue-todo2/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoFooter></TodoFooter>
+    <TodoList></TodoList>
+
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoFooter from './components/TodoFooter.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+
+// var my_cmp = {
+//   template: '<div>my component</div>'
+// }
+
+// new Vue({
+//   el: '',
+//   components: {
+//     'my_cmp': my_cmp
+//   }
+// })
+
+export default {
+  components: {
+    // 컴포넌트 태그명 : 컴포넌트 내용
+    'TodoHeader' : TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoFooter' : TodoFooter,
+    'TodoList' : TodoList
+  }
+
+}
+</script>
+
+<style>
+
+</style>
